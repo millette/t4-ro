a1ce7d9bb1b5801b89cf4b2dc22e74269fbe58ab
Robin Millette <robin@millette.info>
2019-11-08 01:53:30 -0500

    Prettier index file.

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Link from "next/link"
 
 export default () => (
   <p>
-    We've got to get on with the show. <Link href='/custom/categories'><a>stuff...</a></Link>
+    We've got to get on with the show.{" "}
+    <Link href="/custom/categories">
+      <a>stuff...</a>
+    </Link>
   </p>
 )
-
